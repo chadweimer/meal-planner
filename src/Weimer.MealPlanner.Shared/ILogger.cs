2b896ce9f3dc7c02b002a72b63704680b7ffcea8
Chad Weimer <chadweimer@gmail.com>
2015-05-16 12:21:27 -0700

    Upgraded to .NET 4.6 and VS 2015.

--- src/Weimer.MealPlanner.Shared/ILogger.cs
+++ src/Weimer.MealPlanner.Shared/ILogger.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Weimer.MealPlanner.Shared
+﻿namespace Weimer.MealPlanner.Shared
 {
     public interface ILogger
     {
