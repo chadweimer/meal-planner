f5342f9e192775dced6262d783c8cd248c541eb0
Chad Weimer <chadweimer@gmail.com>
2015-05-16 15:42:07 -0700

    Adding files that were apparently missed by the "git mv Weimer.MealPlanner.Shared Weimer.MealPlanner.Core" command.

---
+++ src/Weimer.MealPlanner.Core/Model/Ingredient.cs
@@ -0,0 +1,26 @@
+﻿using QuantityTypes;
+
+namespace Weimer.MealPlanner.Core.Model
+{
+    /// <summary>
+    /// Represents an ingredient in a recipe.
+    /// </summary>
+    public class Ingredient
+    {
+        /// <summary>
+        /// Gets or sets the name of the ingredient.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the quantity of the ingredient.
+        /// </summary>
+        /// <value>
+        /// The quantity.
+        /// </value>
+        public IQuantity Quantity { get; set; }
+    }
+}
---
+++ src/Weimer.MealPlanner.Core/Model/Recipe.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace Weimer.MealPlanner.Core.Model
+{
+    /// <summary>
+    /// Represents a recipe.
+    /// </summary>
+    public class Recipe
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Recipe"/> class.
+        /// </summary>
+        public Recipe()
+        {
+            Ingredients = new List<Ingredient>();
+            Directions = new List<string>();
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the recipe.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a description of the recipe.
+        /// </summary>
+        /// <value>
+        /// The description.
+        /// </value>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets a collection of <see cref="Ingredient"/> instances in this recipe.
+        /// </summary>
+        /// <value>
+        /// The ingredients.
+        /// </value>
+        public IList<Ingredient> Ingredients { get; private set; }
+
+        /// <summary>
+        /// Gets a collection of directions or steps to perform the recipe.
+        /// </summary>
+        /// <value>
+        /// The directions.
+        /// </value>
+        public IList<string> Directions { get; private set; }
+    }
+}
